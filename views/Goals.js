3bf6ba018d8846cbd6de600584a92ddad5a60694
David B <54827653+dmbeta@users.noreply.github.com>
2022-09-24 14:14:55 -0500

    for some reason, today is included in less than

--- views/Goals.js
+++ views/Goals.js
@@ -5,12 +5,29 @@ import { StyleSheet, Text, View } from "react-native";
 import Expansion from "../components/Expansion";
 import { db } from "../util/firebase";
 import { doc, getDoc } from "firebase/firestore";
+import { focusProps } from "react-native-web/dist/cjs/modules/forwardedProps";
 
 const Goals = () => {
   const [goals, setGoals] = useState({});
-  const [dates, setDates] = useState([]);
+  const [todays, setTodays] = useState([]);
+  const [yesterdays, setYesterdays] = useState([]);
+  const [tomorrows, setTomorrows] = useState([]);
 
   useEffect(() => {
+    function formatDate(date) {
+      var d = new Date(date),
+        month = "" + (d.getMonth() + 1),
+        day = "" + d.getDate(),
+        year = d.getFullYear();
+
+      if (month.length < 2) month = "0" + month;
+      if (day.length < 2) day = "0" + day;
+
+      return [year, month, day].join("-");
+    }
+    function lessthan(Date1, Date2) {
+      return new Date(Date1).getTime() < new Date(Date2).getTime();
+    }
     async function getGoals() {
       const allGoals = {};
       const docRef = doc(db, "patient", global.config.patientId);
@@ -27,10 +44,25 @@ const Goals = () => {
           }
           allGoals[goalDate] = [...allGoals[goalDate], obj];
         }
+        const dates_less_than = Object.keys(allGoals).filter((key) =>
+          lessthan(Date.parse(key), new Date())
+        );
+        const dates_greater_than = Object.keys(allGoals).filter((key) =>
+          lessthan(new Date(), Date.parse(key))
+        );
+        console.log("Keys");
+        console.log(Object.keys(allGoals));
+        console.log(dates_less_than);
+        console.log(dates_greater_than);
+        setTodays(allGoals[formatDate(new Date())]);
+        setTomorrows();
+        setYesterdays();
         setGoals(allGoals);
-        setDates(Object.keys(allGoals));
-        console.log(allGoals);
-        console.log(dates);
+        // console.log(allGoals);
+        console.log(yesterdays);
+        console.log(todays);
+        console.log(tomorrows);
+        // console.log(dates);
       } else {
         console.log("No such document!");
       }
@@ -40,30 +72,19 @@ const Goals = () => {
 
   return (
     <View>
-      {dates.map((date, index) => {
-        return (
-          <View>
-            <Text key={date + index}>{date}</Text>
-            {goals[date].map((task, task_index) => (
-              <Text key={task.id + date}>{task.goal}</Text>
-            ))}
-          </View>
-        );
-      })}
-      {/* <View style={styles.container}>
-          <Text style={styles.title}>Today</Text>
-          <CheckList tasks={goals} setTasks={() => setTasks}></CheckList>
-        </View>
-        <View style={styles.container}>
-          <Text style={styles.title}>Upcoming Goals</Text>
-          <CheckList tasks={tasks} setTasks={() => setTasks}></CheckList>
-          <Expansion
+      <View style={styles.container}>
+        <Text style={styles.title}>Today</Text>
+        <Text>where today's will go</Text>
+      </View>
+      <View style={styles.container}>
+        <Text style={styles.title}>Upcoming Goals</Text>
+        {/* <Expansion
             title="Expansion Title"
             child={() => (
               <CheckList tasks={tasks} setTasks={() => setTasks}></CheckList>
             )}
-          ></Expansion>
-        </View> */}
+          ></Expansion> */}
+      </View>
     </View>
   );
 };
