2385edd20498048a6827d5df24be294eea9a9e64
SaloniSS <sxs170015@utdallas.edu>
2022-09-24 17:50:18 -0500

    accordian styling

--- components/Expansion.js
+++ components/Expansion.js
@@ -1,26 +1,30 @@
 import React, { useState } from "react";
 import { List } from "react-native-paper";
 import { Pressable, View, StyleSheet, Text } from "react-native";
+import { ChevronRightIcon, ChevronDownIcon } from "native-base";
 
 function Expansion({ child, title }) {
   const [isExpanded, expand] = useState(false);
   async function show() {
     expand(!isExpanded);
   }
+
   return (
     <View>
       <List.Accordion
         title={title}
-        left={(props) => (
-          <List.Icon
-            {...props}
-            icon={isExpanded ? "chevron-right" : "chevron-down"}
-          />
-        )}
+        left={
+          isExpanded
+            ? (props) => <ChevronRightIcon />
+            : (props) => <ChevronDownIcon />
+        }
         right={(props) => null}
         expanded={isExpanded}
         onPress={show}
         style={styles.container}
+        titleStyle={{
+          color: "black",
+        }}
       >
         {child()}
       </List.Accordion>
@@ -32,6 +36,8 @@ const styles = StyleSheet.create({
   container: {
     width: "100%",
     alignItems: "center",
+    backgroundColor: "#F5DCDA",
+    padding: 10,
   },
   button: {
     marginBottom: 30,
