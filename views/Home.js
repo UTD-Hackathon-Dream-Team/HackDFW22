98e092db8cb0d93eb6f8017c020caaf26a8a88ef
SaloniSS <sxs170015@utdallas.edu>
2022-09-24 11:32:36 -0500

    goal list

--- views/Home.js
+++ views/Home.js
@@ -1,14 +1,16 @@
 import React, { useState, useEffect } from "react";
-import { Container, Center, Text, FlatList, Checkbox } from "native-base";
+import { Container, Center, Text, FlatList, View } from "native-base";
+import { Checkbox, ListItem, Body } from "react-native";
 import { db } from "../util/firebase";
-import { doc, getDoc } from "firebase/firestore";
+import { doc, getDoc, updateDoc } from "firebase/firestore";
+import Icon from "react-native-vector-icons/MaterialIcons";
 
 export default function Home() {
   const [goals, setGoals] = useState({});
+  var today = new Date().toJSON().slice(0, 10);
 
   useEffect(() => {
     async function getGoals() {
-      var today = new Date().toJSON().slice(0, 10);
       const docRef = doc(db, "patient", global.config.patientId);
       const docSnap = await getDoc(docRef);
       if (docSnap.exists()) {
@@ -41,6 +43,22 @@ export default function Home() {
     getGoals();
   }, []);
 
+  async function completeGoal(goalID) {
+    console.log("Updating", goalID);
+    const docRef = doc(db, "patient", global.config.patientId);
+    const docSnap = await getDoc(docRef);
+    var oldGoals = await docSnap.data().goals;
+    var objIndex = oldGoals.findIndex((obj) => obj.id == goalID);
+    console.log(objIndex);
+    console.log(oldGoals[objIndex]);
+    oldGoals[objIndex].done = !oldGoals[objIndex].done;
+    const results = oldGoals.filter((obj) => {
+      var goalDate = new Date(obj.date.seconds * 1000).toJSON().slice(0, 10);
+      return goalDate === today;
+    });
+    setGoals(results);
+  }
+
   return (
     <Center>
       <Container>
@@ -49,7 +67,17 @@ export default function Home() {
         <FlatList
           data={goals}
           style={{ backgroundColor: "pink", height: 150, flexGrow: 0 }}
-          renderItem={({ item }) => <Checkbox>{item.goal}</Checkbox>}
+          renderItem={({ item }) => (
+            <View>
+              <Icon
+                name={item.done ? "check-circle" : "radio-button-unchecked"}
+                size={20}
+                color="#666666"
+                onPress={() => completeGoal(item.id)}
+              />
+              <Text>{item.goal}</Text>
+            </View>
+          )}
         />
         <Text>Information</Text>
         {/* <FlatList
