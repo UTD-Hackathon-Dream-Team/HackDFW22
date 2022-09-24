3d6f5230becba285f3fede83aeffe00620044e32
SaloniSS <sxs170015@utdallas.edu>
2022-09-24 03:14:24 -0500

    login screen

--- views/Login.js
+++ views/Login.js
@@ -1,17 +1,55 @@
 import React from "react";
 import { Button, Center, Container } from "native-base";
+import { View, TextInput, Text } from "react-native";
 
 export default function Home({ navigation }) {
+  const [name, onChangeName] = React.useState(null);
+  const [dob, onChangeDOB] = React.useState("");
+
+  function login() {
+    console.log(name, dob);
+    // navigation.navigate("Root");
+  }
+
   return (
-    <Center>
+    <View
+      style={{
+        flex: 1,
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
       <Container>
-        <Button shadow={2} onPress={() => navigation.navigate("Root")}>
-          Click me
-        </Button>
-        <Button shadow={2} onPress={() => navigation.navigate("Root")}>
-          Click me
+        <Text style={{ fontSize: 20, marginBottom: 10 }}>Name</Text>
+        <TextInput
+          style={{
+            height: 40,
+            borderColor: "gray",
+            borderWidth: 1,
+            width: 200,
+            marginBottom: 10,
+          }}
+          onChangeText={onChangeName}
+          value={name}
+        />
+        <Text style={{ fontSize: 20, marginBottom: 10 }}>Date of Birth</Text>
+        <TextInput
+          style={{
+            height: 40,
+            borderColor: "gray",
+            borderWidth: 1,
+            width: 200,
+            marginBottom: 10,
+          }}
+          onChangeText={onChangeDOB}
+          value={dob}
+          placeholder="MMDDYYYY"
+          placeholderTextColor="grey"
+        />
+        <Button shadow={2} onPress={login}>
+          Log In
         </Button>
       </Container>
-    </Center>
+    </View>
   );
 }
